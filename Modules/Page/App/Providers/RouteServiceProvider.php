78fd6feffa4f739d09bd5079877231f8d81580b8
yamata <codeindevelopment@gmail.com>
2025-06-03 22:37:56 +0330

    feat: implement page management with CRUD operations and SEO/schema integration

---
+++ Modules/Page/App/Providers/RouteServiceProvider.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Modules\Page\App\Providers;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * The module namespace to assume when generating URLs to actions.
+     */
+    protected string $moduleNamespace = 'Modules\Page\App\Http\Controllers';
+
+    /**
+     * Called before routes are registered.
+     *
+     * Register any model bindings or pattern based filters.
+     */
+    public function boot(): void
+    {
+        parent::boot();
+    }
+
+    /**
+     * Define the routes for the application.
+     */
+    public function map(): void
+    {
+        $this->mapUserApiRoutes();
+
+        $this->mapAdminApiRoutes();
+    }
+
+    // User API Routes
+    protected function mapUserApiRoutes(): void
+    {
+        Route::prefix('api')
+            ->middleware('api')
+            ->namespace($this->moduleNamespace)
+            ->group(module_path('Page', '/routes/API_user.php'));
+    }
+
+    // Admin Api Routes
+    protected function mapAdminApiRoutes(): void
+    {
+        Route::prefix('api')
+            ->middleware('api')
+            ->namespace($this->moduleNamespace)
+            ->group(module_path('Page', '/routes/API_admin.php'));
+    }
+}
